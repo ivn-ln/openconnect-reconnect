22a647c6d32da6b183282bf03ba73e5bc3b01e90
ivn-ln <illarn@ya.ru>
2025-05-19 23:11:12 +0300

    fix: new script logic

--- vpn.bash
+++ vpn.bash
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-if [[ $# < 1 ]] then
+if [[ $# -lt 1 ]]; then
 	echo "Usage: $0 <IP_ADDRESS> [USER] [PASSWORD] [CERTIFICATE]"
-	exit
+	exit 1
 fi
 
 # Credentials
@@ -12,32 +12,40 @@ VPN_PASS="${3:-replace this}"
 
 # Regex pattern for output to ignore
 FILTER_PATTERN="replace this"
-# Regex pattern to reconnect on
-RECONNECT_PATTERN="CSTP Dead Peer Detection detected dead peer!|DTLS handshake failed: Resource temporarily unavailable, try again."
+
+# Timeout time for pings
+TIMEOUT_TIMER=10
 
 while true; do
+	exit_flag=false
+	
 	# Connection
 	echo "$VPN_PASS" | sudo openconnect "$1" \
-	-u "$VPN_USER" \
-	--servercert "$VPN_CERT" \
-	2>&1 | {
-		while read -r line; do
+		-u "$VPN_USER" \
+		--servercert "$VPN_CERT" \
+		2>&1 | while read -r line; do
 			# Check whether to filter
-			echo "$line" | grep -qP "$FILTER_PATTERN"
-			if [[ $? == 1 ]]; then
+			if ! echo "$line" | grep -qP "$FILTER_PATTERN"; then
 				echo "$line"
 			fi
-			# Check whether to reconnect
-			if echo "$line" | grep -qP "$RECONNECT_PATTERN"; then
-				break
-			fi
-		done
-	}
-	# Exit on SIGINT, otherwise reconnect
-	if [[ "$?" != 130 ]] then
-		echo -e "\nReconnecting...\n"
-		sleep 5
+		done &
+	
+	VPN_PID=$!
+
+	# Check connection every TIMEOUT_TIMER seconds
+	while [[ $(nmcli networking connectivity) == "full" ]]; do
+		if ! ping -c 1 -W $TIMEOUT_TIMER 8.8.8.8 &>/dev/null; then
+			echo -e "\n Restarting VPN..."
+			exit_flag=true
+			kill "$VPN_PID"
+			break
+		fi
+	done
+
+	if $exit_flag; then
+		echo -e "\nReconnecting to VPN...\n"
 	else
+		echo "Exiting"
 		break
 	fi
 done
