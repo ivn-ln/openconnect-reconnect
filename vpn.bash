888bb8fa3f8eb171eca7ccc0bc06e3eb75c79f8b
ivn-ln <illarn@ya.ru>
2025-05-21 01:34:36 +0300

    fix: fixed script

--- vpn.bash
+++ vpn.bash
@@ -1,51 +1,55 @@
 #!/bin/bash
+set -e
 
-if [[ $# < 1 ]]; then
+if [[ $# -lt 1 ]]; then
 	echo "Usage: $0 <IP_ADDRESS> [USER] [PASSWORD] [CERTIFICATE]"
 	exit 1
 fi
 
 # Credentials
-VPN_USER="${2:-replace this}"
-VPN_CERT="${4:-replace this}"
-VPN_PASS="${3:-replace this}"
+VPN_USER="${2:-REPLACE THIS}"
+VPN_CERT="${4:-REPLACE THIS}"
+VPN_PASS="${3:-REPLACE THIS}"
 
-# Regex pattern for output to ignore
-FILTER_PATTERN="replace this"
+TIMEOUT_TIME=5
 
-# Timeout time for pings
-TIMEOUT_TIMER=10
+# FIFO for the password
+PIPE_PATH=/tmp/vpn
+if [[ ! -e "$PIPE_PATH" ]]; then
+	mkfifo $"PIPE_PATH"
+fi
+
+# Kill all openconnect instances (mostly for debug purposes) and remove the pipe
+cleanup() {
+	pkill openconnect
+	rm -f "$PIPE_PATH"
+	exit
+}
+
+# Clean up on exit, exit on SIGINT
+trap "echo \"Cleaning up\" $(cleanup)" EXIT
+trap exit INT
 
 while true; do
-	exit_flag=false
-	
 	# Connection
-	echo "$VPN_PASS" | sudo openconnect "$1" \
-		-u "$VPN_USER" \
-		--servercert "$VPN_CERT" \
-		2>&1 | while read -r line; do
-			# Check whether to filter
-			if ! echo "$line" | grep -qP "$FILTER_PATTERN"; then
-				echo "$line"
-			fi
-		done &
-	
+	echo "$VPN_PASS" > "$PIPE_PATH" &
+	openconnect "$1" \
+	-u "$VPN_USER" \
+	--servercert "$VPN_CERT" \
+	--passwd-on-stdin < "$PIPE_PATH" 2>&1 &
+
 	VPN_PID=$!
 
 	# Check connection every TIMEOUT_TIMER seconds
-	while [[ $(nmcli networking connectivity) == "full" ]]; do
-		if ! ping -c 1 -W $TIMEOUT_TIMER 8.8.8.8 &>/dev/null; then
-			echo -e "\n Restarting VPN..."
-			exit_flag=true
-			kill "$VPN_PID"
+	while kill -0 "$VPN_PID" 2>/dev/null; do
+		if ! ping -c 1 -W "$TIMEOUT_TIME" 8.8.8.8 &> /dev/null; then
+			echo -e "\nConnection dropped!"
+			kill -2 "$VPN_PID"
+			wait "$VPN_PID"
 			break
 		fi
 	done
 
-	if $exit_flag; then
-		echo -e "\nReconnecting to VPN...\n"
-	else
-		echo "Exiting"
-		break
-	fi
+	echo -e "Reconnecting to VPN...\n"
+	sleep 1
 done
